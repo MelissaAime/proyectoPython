57b724d59b0f0ee44e6b03febb142702e88aec3f
Melissa Aime <aimemelissa1@gmail.com>
2022-07-27 19:00:17 -0300

    first mvt project

--- mi_proyecto/familiares/views.py
+++ mi_proyecto/familiares/views.py
@@ -6,17 +6,6 @@ from familiares.models import Familia
 
 def mi_familia(request):
     familia = Familia.objects.all()
-
-# Sin conexion con el template
-    # listado_nombres= []
-
-    # for f in familia:
-    #     listado_nombres.append(f"Familiar: {f.id}, Nombre: {f.nombre}, Edad: {f.edad}, Parentezco: {f.parentezco}, ")
-
-    # return HttpResponse(listado_nombres)
-
-
-# Conectando con el template, no funciona:
         
     plantilla = loader.get_template("familiares.html")
 
